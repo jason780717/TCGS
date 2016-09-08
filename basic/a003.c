31017174084e6ffede3656cfd7c4e6e66ed25652
Huang Fu Wen <jason780717@gmail.com>
2016-09-08 14:22:02 +0800

    add a003.c

---
+++ basic/a003.c
@@ -0,0 +1,18 @@
+/**********************************************************************************/  
+/*  Problem: a003 "許願" from 跳脫字元 \                                                */  
+/*  Language: C++                                                                 */  
+/*  Result: AC (5ms, 568KB) on ZeroJudge                                          */  
+/*  Author: jason780717 at 2013-06-08 22:38:19                                    */  
+/**********************************************************************************/  
+  
+#include <stdio.h>                
+#include <stdlib.h>                
+int main(void)                
+{                
+     printf("^_^\n");                
+     printf("\"o\"\n");      
+     printf("\\^o^/\n");      
+     system("pause");                
+     return 0;                
+}              
+

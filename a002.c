e74557c9d8e012a2247959f2c307357667bf2156
Huang Fu Wen <jason780717@gmail.com>
2016-09-08 12:01:55 +0800

    add a002.c

---
+++ a002.c
@@ -0,0 +1,16 @@
+/**********************************************************************************/  
+/*  Problem: a002 "This is a book. (*)" from 換行符號 endl                        */  
+/*  Language: C++                                                                 */  
+/*  Result: AC (5ms, 568KB) on ZeroJudge                                          */  
+/*  Author: jason780717 at 2013-06-08 22:20:14                                    */  
+/**********************************************************************************/  
+  
+#include <stdio.h>    
+#include <stdlib.h>    
+int main(void)    
+{    
+     printf("This is a book.\n That is a pen.\n I am a student.\n");    
+    
+     system("pause");    
+     return 0;    
+}    
